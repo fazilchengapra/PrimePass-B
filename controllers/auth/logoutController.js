dcf1854d7a5cced5802494649fd0e1549628abf5
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-10-01 13:34:11 +0530

    Fix cookie SameSite attribute casing for consistency across auth controllers, fixed social login bug

--- controllers/auth/logoutController.js
+++ controllers/auth/logoutController.js
@@ -2,7 +2,7 @@ exports.logout = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // only https in production
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "Lax",
   });
 
   return res.json({ success: true, message: "Logged out successfully" });
--- controllers/auth/oauthController.js
+++ controllers/auth/oauthController.js
@@ -1,11 +1,31 @@
-const { verifyGoogleUser, loginUser } = require("../../services/authService");
+const { verifyGoogleUser } = require("../../services/authService");
 const sendResponse = require("../../utils/sendResponse");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.googleOauth = async (req, res) => {
   try {
     const { code } = req.body;
     const user = await verifyGoogleUser(code);
-    loginUser(res, user);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "Lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      })
+      .status(200)
+      .json({
+        message: "Login successful",
+        user: {
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+        },
+      });
   } catch (error) {
     // console.log("error:", error);
     sendResponse(res, 400, "Something went wrong!", false);
