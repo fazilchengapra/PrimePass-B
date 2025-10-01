9cba870b0f5c67dd0125c620388dadae29c8ae02
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-10-01 17:55:37 +0530

    Add verification check for user email status in loginUser function

--- services/authService.js
+++ services/authService.js
@@ -14,6 +14,8 @@ exports.loginUser = async (res, userData) => {
   const user = await User.findOne({ email });
   if (!user) throw new Error("Email or password incorrect");
 
+  if (!user.isVerified) throw new Error("Email or password incorrect");
+
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) throw new Error("Email or password incorrect");
 
